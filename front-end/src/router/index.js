74e2964ebc79f71c0f60747398981e6be7e9b424
HeyImParker <patrickjpark@gmail.com>
2021-04-05 20:33:08 -0600

    small fix

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Item from '../views/Item.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
